fa2b30feceed774cd7ddad15f6b9a0b553d12528
guilhem ALBALADEJO <guilhem.albaladejo@gmail.com>
2025-06-13 01:42:41 +0200

    disable blocking mode for user input

--- terminal_settings.hpp
+++ terminal_settings.hpp
@@ -3,27 +3,34 @@
 
 #include <termios.h>
 #include <unistd.h>
+#include <fcntl.h>
 
 // wrapper class for changing terminal mode
 class TerminalSettings {
 public:
     TerminalSettings() {
         if(!initialized_) {
-            tcgetattr(STDIN_FILENO, &oldt_);
+            tcgetattr(STDIN_FILENO, &oldt_); // save flags
             termios newt = oldt_;
-            newt.c_lflag &= ~(ICANON | ECHO);
+            newt.c_lflag &= ~(ICANON | ECHO); // flags are : no waiting for enter, no echo, and no waiting for input (in read())
             tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+
+            oldf_ = fcntl(STDIN_FILENO, F_GETFL, 0); // save file status
+            fcntl(STDIN_FILENO, F_SETFL, oldf_ | O_NONBLOCK);
             initialized_ = true;
         }
     }
     ~TerminalSettings() {
         restoreSettings();
     }
-    static inline void restoreSettings() {
-        tcsetattr(STDIN_FILENO, TCSANOW, &oldt_);  // Restores original settings
+    static inline void restoreSettings() { // Reloads old settings
+        tcsetattr(STDIN_FILENO, TCSANOW, &oldt_);
+        fcntl(STDIN_FILENO, F_SETFL, oldf_);  
+
     }
 private:
-    inline static termios oldt_; // original settings
+    inline static int oldf_; // original file descriptor settings
+    inline static termios oldt_; // original terminal settings
     inline static bool initialized_ = false;
 };
 

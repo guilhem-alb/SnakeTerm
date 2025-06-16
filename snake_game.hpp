ff1032ed17a3c55a96b6766a1de1e9c2013057f0
guilhem ALBALADEJO <guilhem.albaladejo@gmail.com>
2025-06-16 15:25:58 +0200

    changed visibility of game_board member

--- snake_game.hpp
+++ snake_game.hpp
@@ -12,11 +12,13 @@ public:
     int getScore() {return score_;};
     void updateGame();
     void endGame();
-    Board game_board_;
+    
 private:
     int score_ = 0;
     
+    Board game_board_;
     Snake player_snake_;
+
     // key recognition handling
     Direction mapKey(char input[], int input_size); // returns the key associated with the input string
     void setCursor();
